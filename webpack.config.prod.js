2489e5fcc192948eaff2da1b71ad3516babb7f15
mfriedberg <mfriedberg@netflix.com>
2020-05-11 01:19:13 -0700

    Prod Build Module WIP

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -1,19 +1,31 @@
 import webpack from 'webpack';
 import path from 'path';
+import UglifyJsPlugin from 'uglifyjs-webpack-plugin'
+
+/* const UglifyJsPlugin = require('uglifyjs-webpack-plugin'); */
 
 export default {
-  devtool: 'inline-source-map',
-  mode: 'development',
+  devtool: 'source-map',
+  mode: 'production',
   entry: [
     path.resolve(__dirname, 'src/index')
   ],
   target: 'web',
   output: {
-    path: path.resolve(__dirname, 'src'),
+    path: path.resolve(__dirname, 'dist'),
     publicPath: '/',
     filename: 'bundle.js'
   },
+  optimization: {
+    minimizer: [new UglifyJsPlugin()]
+  },
   plugins: [
+    //Eliminate deplicate packages when generating bundle
+    /* new webpack.optimize.DedupePlugin(), */
+
+    // Minify JS
+    /* new webpack.optimize.UglifyJsPlugin(), */
+    
     new webpack.LoaderOptionsPlugin({
       debug: true,
       noInfo: false,
